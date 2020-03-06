6ed10442e01c7db9ac3a0b9acbb8d19bb1cac695
Rima O <rymaothmani@gmail.com>
2020-03-05 23:29:38 -0600

    debug html route not updating

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,27 +1,13 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
-var path = require("path");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+const path = require("path");
 
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../develop/index.html"));
-  });
 
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../develop/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-
+  
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
   
 };
