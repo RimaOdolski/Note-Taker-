f17de0a0e45e7de87f9506f590faf57b007d6c74
Rima O <rymaothmani@gmail.com>
2020-03-07 12:14:57 -0600

    debug apiroutes

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
-require("./routes/api-routes")(app);
-require("./routes/html-routes")(app);
+require("./routes/apiroutes.js")(app);
+require("./routes/htmlroutes.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
